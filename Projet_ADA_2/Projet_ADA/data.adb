8793c703ebc6cf30f53037f90e99303d29efca1e
Hyriden <45774188+Hyriden@users.noreply.github.com>
2018-12-15 22:31:19 +0100

    Add files via upload

--- Projet_ADA_2/Projet_ADA/data.adb
+++ Projet_ADA_2/Projet_ADA/data.adb
@@ -55,6 +55,7 @@ Planning(0)(mercredi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(0)(mercredi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(0)(mercredi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(0)(mercredi,1).cout_prestation:=350;
+Planning(0)(mercredi,1).existe:=true;
 
 Planning(0)(jeudi,1).nom_client:=('B','o','n','t','e','m','p','s',others=>' ');
 Planning(0)(jeudi,1).prenom_client:=('A','n','a','e','l','l','e',others=>' ');
@@ -63,6 +64,8 @@ Planning(0)(jeudi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(0)(jeudi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(0)(jeudi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(0)(jeudi,1).cout_prestation:=510;
+Planning(0)(jeudi,1).existe:=true;
+
 
 -- S1 --
 Planning(1)(mercredi,1).nom_client:=('G','o','u','r','m','a','n','d',others=>' ');
@@ -72,6 +75,8 @@ Planning(1)(mercredi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(1)(mercredi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(1)(mercredi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(1)(mercredi,1).cout_prestation:=255;
+Planning(1)(mercredi,1).existe:=true;
+
 
 Planning(1)(vendredi,1).nom_client:=('P','e','r','s','o','n','n','e',others=>' ');
 Planning(1)(vendredi,1).prenom_client:=('P','a','u','l',others=>' ');
@@ -80,6 +85,8 @@ Planning(1)(vendredi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(1)(vendredi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(1)(vendredi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(1)(vendredi,1).cout_prestation:=210;
+Planning(1)(vendredi,1).existe:=true;
+
 
 -- S2 --
 Planning(2)(mercredi,1).nom_client:=('G','o','u','r','m','a','n','d',others=>' ');
@@ -89,6 +96,8 @@ Planning(2)(mercredi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(2)(mercredi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(2)(mercredi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(2)(mercredi,1).cout_prestation:=255;
+Planning(2)(mercredi,1).existe:=true;
+
 
 Planning(2)(vendredi,1).nom_client:=('V','i','v','a','n','t',others=>' ');
 Planning(2)(vendredi,1).prenom_client:=('J','u','l','i','e','n',others=>' ');
@@ -97,6 +106,8 @@ Planning(2)(vendredi,1).specialite:=cuisine_francaise_traditionnelle;
 Planning(2)(vendredi,1).nom_cuisinier:=('B','o','n','g','r','i','n',others=>' ');
 Planning(2)(vendredi,1).prenom_cuisinier:=('M','a','r','c',others=>' ');
 Planning(2)(vendredi,1).cout_prestation:=510;
+Planning(2)(vendredi,1).existe:=true;
+
 
 	-- JULIE GUSTINE --
 
@@ -108,6 +119,8 @@ Planning(0)(mercredi,2).specialite:=cuisine_francaise_traditionnelle;
 Planning(0)(mercredi,2).nom_cuisinier:=('G','u','s','t','i','n','e',others=>' ');
 Planning(0)(mercredi,2).prenom_cuisinier:=('J','u','l','i','e',others=>' ');
 Planning(0)(mercredi,2).cout_prestation:=350;
+Planning(0)(mercredi,2).existe:=true;
+
 
 Planning(0)(jeudi,2).nom_client:=('B','o','n','p','a','i','n',others=>' ');
 Planning(0)(jeudi,2).prenom_client:=('B','e','r','n','a','r','d',others=>' ');
@@ -116,6 +129,8 @@ Planning(0)(jeudi,2).specialite:=cuisine_francaise_traditionnelle;
 Planning(0)(jeudi,2).nom_cuisinier:=('G','u','s','t','i','n','e',others=>' ');
 Planning(0)(jeudi,2).prenom_cuisinier:=('J','u','l','i','e',others=>' ');
 Planning(0)(jeudi,2).cout_prestation:=210;
+Planning(0)(jeudi,2).existe:=true;
+
 
 Planning(0)(vendredi,2).nom_client:=('P','i','e','r','r','e',others=>' ');
 Planning(0)(vendredi,2).prenom_client:=('P','a','u','l',others=>' ');
@@ -124,6 +139,8 @@ Planning(0)(vendredi,2).specialite:=cuisine_francaise_traditionnelle;
 Planning(0)(vendredi,2).nom_cuisinier:=('G','u','s','t','i','n','e',others=>' ');
 Planning(0)(vendredi,2).prenom_cuisinier:=('J','u','l','i','e',others=>' ');
 Planning(0)(vendredi,2).cout_prestation:=165;
+Planning(0)(vendredi,2).existe:=true;
+
 
 -- S1 --
 Planning(1)(vendredi,2).nom_client:=('Q','u','e','e','n',others=>' ');
@@ -133,6 +150,8 @@ Planning(1)(vendredi,2).specialite:=cuisine_francaise_traditionnelle;
 Planning(1)(vendredi,2).nom_cuisinier:=('G','u','s','t','i','n','e',others=>' ');
 Planning(1)(vendredi,2).prenom_cuisinier:=('J','u','l','i','e',others=>' ');
 Planning(1)(vendredi,2).cout_prestation:=210;
+Planning(1)(vendredi,2).existe:=true;
+
 
 -- S2 --
 Planning(2)(mercredi,2).nom_client:=('J','e','f','a','i','n',others=>' ');
@@ -142,6 +161,8 @@ Planning(2)(mercredi,2).specialite:=cuisine_francaise_traditionnelle;
 Planning(2)(mercredi,2).nom_cuisinier:=('G','u','s','t','i','n','e',others=>' ');
 Planning(2)(mercredi,2).prenom_cuisinier:=('J','u','l','i','e',others=>' ');
 Planning(2)(mercredi,2).cout_prestation:=165;
+Planning(2)(mercredi,2).existe:=true;
+
 
 	-- PIERRE TALAVENT --
 	
@@ -157,6 +178,8 @@ Planning(0)(jeudi,4).specialite:=cuisine_vegetarienne;
 Planning(0)(jeudi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(0)(jeudi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(0)(jeudi,4).cout_prestation:=210;
+Planning(0)(jeudi,4).existe:=true;
+
 
 Planning(0)(vendredi,4).nom_client:=('F','r','u','c','t','i','s',others=>' ');
 Planning(0)(vendredi,4).prenom_client:=('R','o','b','e','r','t',others=>' ');
@@ -165,6 +188,8 @@ Planning(0)(vendredi,4).specialite:=cuisine_vegetarienne;
 Planning(0)(vendredi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(0)(vendredi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(0)(vendredi,4).cout_prestation:=310;
+Planning(0)(vendredi,4).existe:=true;
+
 
 Planning(0)(samedi,4).nom_client:=('N','o','c','a','r','n','e',others=>' ');
 Planning(0)(samedi,4).prenom_client:=('R','a','y','m','o','n','d',others=>' ');
@@ -173,6 +198,8 @@ Planning(0)(samedi,4).specialite:=cuisine_vegetarienne;
 Planning(0)(samedi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(0)(samedi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(0)(samedi,4).cout_prestation:=255;
+Planning(0)(samedi,4).existe:=true;
+
 
 -- S1 --
 Planning(1)(jeudi,4).nom_client:=('V','e','r','d','u','r','e','t','t','e',others=>' ');
@@ -182,6 +209,8 @@ Planning(1)(jeudi,4).specialite:=cuisine_vegetarienne;
 Planning(1)(jeudi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(1)(jeudi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(1)(jeudi,4).cout_prestation:=120;
+Planning(1)(jeudi,4).existe:=true;
+
 
 Planning(1)(samedi,4).nom_client:=('P','a','q','u','e','r','e','t','t','e',others=>' ');
 Planning(1)(samedi,4).prenom_client:=('C','l','o','t','i','l','d','e',others=>' ');
@@ -190,6 +219,8 @@ Planning(1)(samedi,4).specialite:=cuisine_vegetarienne;
 Planning(1)(samedi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(1)(samedi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(1)(samedi,4).cout_prestation:=210;
+Planning(1)(samedi,4).existe:=true;
+
 
 -- S2 --
 Planning(2)(jeudi,4).nom_client:=('V','e','r','d','u','r','e','t','t','e',others=>' ');
@@ -199,6 +230,8 @@ Planning(2)(jeudi,4).specialite:=cuisine_vegetarienne;
 Planning(2)(jeudi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(2)(jeudi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(2)(jeudi,4).cout_prestation:=255;
+Planning(2)(jeudi,4).existe:=true;
+
 
 Planning(2)(samedi,4).nom_client:=('R','u','p','e','s','t','r','e',others=>' ');
 Planning(2)(samedi,4).prenom_client:=('S','y','l','v','e','t','t','e',others=>' ');
@@ -207,6 +240,8 @@ Planning(2)(samedi,4).specialite:=cuisine_vegetarienne;
 Planning(2)(samedi,4).nom_cuisinier:=('V','e','r','d','e',others=>' ');
 Planning(2)(samedi,4).prenom_cuisinier:=('H','e','l','e','n','e',others=>' ');
 Planning(2)(samedi,4).cout_prestation:=520;
+Planning(2)(samedi,4).existe:=true;
+
 
 	-- VINCENT TRAN --
 
@@ -218,6 +253,8 @@ Planning(1)(jeudi,5).specialite:=cuisine_asiatique;
 Planning(1)(jeudi,5).nom_cuisinier:=('T','r','a','n',others=>' ');
 Planning(1)(jeudi,5).prenom_cuisinier:=('V','i','n','c','e','n','t',others=>' ');
 Planning(1)(jeudi,5).cout_prestation:=210;
+Planning(1)(jeudi,5).existe:=true;
+
 
 Planning(1)(samedi,5).nom_client:=('R','u','p','e','s','t','r','e',others=>' ');
 Planning(1)(samedi,5).prenom_client:=('S','y','l','v','e','t','t','e',others=>' ');
@@ -226,6 +263,8 @@ Planning(1)(samedi,5).specialite:=cuisine_asiatique;
 Planning(1)(samedi,5).nom_cuisinier:=('T','r','a','n',others=>' ');
 Planning(1)(samedi,5).prenom_cuisinier:=('V','i','n','c','e','n','t',others=>' ');
 Planning(1)(samedi,5).cout_prestation:=450;
+Planning(1)(samedi,5).existe:=true;
+
 
 -- S2 --
 Planning(2)(jeudi,5).nom_client:=('V','i','v','a','n','t',others=>' ');
@@ -235,6 +274,8 @@ Planning(2)(jeudi,5).specialite:=cuisine_asiatique;
 Planning(2)(jeudi,5).nom_cuisinier:=('T','r','a','n',others=>' ');
 Planning(2)(jeudi,5).prenom_cuisinier:=('V','i','n','c','e','n','t',others=>' ');
 Planning(2)(jeudi,5).cout_prestation:=210;
+Planning(2)(jeudi,5).existe:=true;
+
 
 Planning(2)(vendredi,5).nom_client:=('R','u','p','e','s','t','r','e',others=>' ');
 Planning(2)(vendredi,5).prenom_client:=('S','y','l','v','e','t','t','e',others=>' ');
@@ -243,6 +284,8 @@ Planning(2)(vendredi,5).specialite:=cuisine_asiatique;
 Planning(2)(vendredi,5).nom_cuisinier:=('T','r','a','n',others=>' ');
 Planning(2)(vendredi,5).prenom_cuisinier:=('V','i','n','c','e','n','t',others=>' ');
 Planning(2)(vendredi,5).cout_prestation:=520;
+Planning(2)(vendredi,5).existe:=true;
+
 
 Planning(2)(samedi,5).nom_client:=('C','h','i','n','e',others=>' ');
 Planning(2)(samedi,5).prenom_client:=('A','l','a','i','n',others=>' ');
@@ -251,6 +294,8 @@ Planning(2)(samedi,5).specialite:=cuisine_asiatique;
 Planning(2)(samedi,5).nom_cuisinier:=('T','r','a','n',others=>' ');
 Planning(2)(samedi,5).prenom_cuisinier:=('V','i','n','c','e','n','t',others=>' ');
 Planning(2)(samedi,5).cout_prestation:=310;
+Planning(2)(samedi,5).existe:=true;
+
 
 	-- SYLVIE BEAUCHAMP --
 
@@ -262,6 +307,7 @@ Planning(0)(mercredi,6).specialite:=buffet;
 Planning(0)(mercredi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(0)(mercredi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(0)(mercredi,6).cout_prestation:=1020;
+Planning(0)(mercredi,6).existe:=true;
 
 Planning(0)(vendredi,6).nom_client:=('L','e','g','r','o','u','p','e',others=>' ');
 Planning(0)(vendredi,6).prenom_client:=('J','u','s','t','i','n','e',others=>' ');
@@ -270,6 +316,8 @@ Planning(0)(vendredi,6).specialite:=buffet;
 Planning(0)(vendredi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(0)(vendredi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(0)(vendredi,6).cout_prestation:=430;
+Planning(0)(vendredi,6).existe:=true;
+
 
 Planning(0)(samedi,6).nom_client:=('Q','u','e','e','n',others=>' ');
 Planning(0)(samedi,6).prenom_client:=('C','h','r','i','s','t','i','n','e',others=>' ');
@@ -278,6 +326,8 @@ Planning(0)(samedi,6).specialite:=buffet;
 Planning(0)(samedi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(0)(samedi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(0)(samedi,6).cout_prestation:=660;
+Planning(0)(samedi,6).existe:=true;
+
 
 -- S1 --
 Planning(1)(samedi,6).nom_client:=('P','o','m','p','e','t','t','e',others=>' ');
@@ -287,6 +337,8 @@ Planning(1)(samedi,6).specialite:=buffet;
 Planning(1)(samedi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(1)(samedi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(1)(samedi,6).cout_prestation:=520;
+Planning(1)(samedi,6).existe:=true;
+
 
 -- S2 --
 Planning(2)(mardi,6).nom_client:=('V','e','r','d','u','r','e','t','t','e',others=>' ');
@@ -296,6 +348,8 @@ Planning(2)(mardi,6).specialite:=buffet;
 Planning(2)(mardi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(2)(mardi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(2)(mardi,6).cout_prestation:=520;
+Planning(2)(mardi,6).existe:=true;
+
 
 Planning(2)(jeudi,6).nom_client:=('P','i','e','r','r','e',others=>' ');
 Planning(2)(jeudi,6).prenom_client:=('P','a','u','l',others=>' ');
@@ -304,6 +358,8 @@ Planning(2)(jeudi,6).specialite:=buffet;
 Planning(2)(jeudi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(2)(jeudi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(2)(jeudi,6).cout_prestation:=870;
+Planning(2)(jeudi,6).existe:=true;
+
 
 Planning(2)(samedi,6).nom_client:=('B','o','n','t','e','m','p','s',others=>' ');
 Planning(2)(samedi,6).prenom_client:=('A','n','a','e','l','l','e',others=>' ');
@@ -312,6 +368,8 @@ Planning(2)(samedi,6).specialite:=buffet;
 Planning(2)(samedi,6).nom_cuisinier:=('B','e','a','u','c','h','a','m','p',others=>' ');
 Planning(2)(samedi,6).prenom_cuisinier:=('S','y','l','v','i','e',others=>' ');
 Planning(2)(samedi,6).cout_prestation:=1581;
+Planning(2)(samedi,6).existe:=true;
+
 
 	-- ARNAUD MINCK --
 
@@ -323,6 +381,8 @@ Planning(1)(samedi,7).specialite:=buffet;
 Planning(1)(samedi,7).nom_cuisinier:=('M','i','n','c','k',others=>' ');
 Planning(1)(samedi,7).prenom_cuisinier:=('A','r','n','a','u','d',others=>' ');
 Planning(1)(samedi,7).cout_prestation:=1020;
+Planning(1)(samedi,7).existe:=true;
+
 
 	end init;
 end data;
